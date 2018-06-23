277ee55b56ac6905e3838e035b806d650d6a509d
Jordan Ide <jorrdandied@hotmail.com>
2018-06-23 18:23:15 +0100

    update feature-box to use flex

---
+++ src/app/routes/home/feature-box/feature-box.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feature-box',
+  templateUrl: './feature-box.component.html',
+  styleUrls: ['./feature-box.component.css']
+})
+export class FeatureBoxComponent implements OnInit {
+
+  @Input() imageUrl: string;
+  @Input() title: string;
+  @Input() content: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/routes/home/home.module.ts
+++ src/app/routes/home/home.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { RouterModule } from '@angular/router';
+import { FeatureBoxComponent } from './feature-box/feature-box.component';
 
 @NgModule({
   imports: [
@@ -10,6 +11,6 @@ import { RouterModule } from '@angular/router';
       {path: '', component: HomeComponent}
     ])
   ],
-  declarations: [HomeComponent]
+  declarations: [HomeComponent, FeatureBoxComponent]
 })
 export class HomeModule { }
